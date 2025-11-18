5d0c9b8a6268d6e243a65a3775cf6eb05c2be4e0
suehtamxx <eldermatheus60@gmail.com>
2025-11-18 00:41:43 -0300

    desgraça que nao funciona

---
+++ data/povoar.py
@@ -0,0 +1,42 @@
+import csv
+import random
+from faker import Faker
+
+fake = Faker('pt_BR')
+
+qtd_pessoas = 10000
+qtd_empresas = 1000
+qtd_relacoes = 50000
+
+pessoas_id = []
+
+with open('data/pessoas.csv', 'w', newline='', encoding='utf-8') as f:
+	'''Função para gerar o arquivo de pessoas'''
+	writer = csv.writer(f)
+	writer.writerow(['id','nome','cidade',])
+	for i in range(qtd_pessoas):
+		id_pessoa = f'p{i}'
+		pessoas_id.append(id_pessoa)
+
+		writer.writerow([id_pessoa, fake.name(), fake.city()])
+
+empresas_id = []
+with open('data/empresas.csv', 'w', newline='', encoding='utf-8') as f:
+	'''Função para gerar o arquivo de empresas'''
+	writer = csv.writer(f)
+	writer.writerow(['id','nome_empresa'])
+	for i in range(qtd_empresas):
+		id_empresa = f'e{i}'
+		empresas_id.append(id_empresa)
+		writer.writerow([id_empresa, fake.company()])
+
+with open('data/relacoes.csv', 'w', newline='', encoding='utf-8') as f:
+	'''Função para gerar o arquivo de relacoes'''
+	writer = csv.writer(f)
+	writer.writerow(['id_pessoa','id_empresa'])
+	for i in range(qtd_relacoes):
+		id_pessoa = random.choice(pessoas_id)
+		id_empresa = random.choice(empresas_id)
+		writer.writerow([id_pessoa, id_empresa])
+
+print('Arquivos CSV gerados\n')
